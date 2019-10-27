ce225d680c38793688640e1091a22f606e628c68
artem kosarev <aionics@list.ru>
2019-10-27 19:21:21 +0300

    user model updated

--- models/user.js
+++ models/user.js
@@ -3,24 +3,13 @@ const {PARTY_CONSTANTS} = require(__base + 'config')
 const user_schema = new Schema({
     id: String,
     money: {type: Number, default: PARTY_CONSTANTS.WELCOME_BONUS},
-    level: {type: Number, default: 1},
-    purchased_items: {type: Array, default: []}
+    level: {type: Number, default: 1}
 })
 
 user_schema.methods.income = function(cb) {
     this.money += PARTY_CONSTANTS.INCOME_AMOUNT
     return cb(null, 'ok')
     this.save()
-}
-
-user_schema.methods.purchase = function(item, cb) {
-    if (this.money < item.cost) {
-        return cb('we require more minerals')
-    }
-    this.money -= item.cost
-    this.purchased_items = this.purchased_items.push[item]
-    this.save()
-    return cb(null, 'ok')
 }
 
 module.exports = model('User', user_schema)
--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const config = require('./config')
 const db = require('./database')
 const User = require('./models/user')
 
-let user = new User({id: 'test'})
-user.purchase({name: 'jopa', cost: 5}, (err, res) => {
-    console.log(err, res)
-})
-console.log(user.purchased_items)
-
-
 app.get('/', (req, res) => {
     res.send('nice to meet u')
 })
