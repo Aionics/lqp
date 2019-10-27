f95e05411c88c57318edd7880fad95dc9fbe921c
Anton Istomin <istomanton@gmail.com>
2019-10-28 00:35:01 +0500

    Add bootstrap-users script, modify User model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,13 @@ const {PARTY_CONSTANTS} = require('../config')
 const user_schema = new Schema({
     id: String,
     money: {type: Number, default: PARTY_CONSTANTS.WELCOME_BONUS},
-    level: {type: Number, default: 1}
+    level: {type: Number, default: 1},
+    isAdmin: {type: Boolean, default: false},
+    displayId: {type: String},
+    displayName: {type: String, default: ""},
+    adminLogin: {type: String},
+    adminPassword: {type: String},
+    secretKey: {type: String}
 })
 
 user_schema.methods.income = function(cb) {
