e10864cf797a47d37a3a69011cb9b8e9f2e815cd
artem kosarev <aionics@list.ru>
2019-11-01 01:59:50 +0300

    population array

--- src/middlewares/admin/getTransactions.ts
+++ src/middlewares/admin/getTransactions.ts
@@ -1,9 +1,10 @@
 import {AppMiddleware} from '../../../typings/koa'
 import {Event} from "../../models/event";
+import {User} from "../../models/user";
 
 export const getTransactions: AppMiddleware = async (ctx, next) => {
     const moneyChanges = await Event.find({moneyChange: {$ne: null}})
-        .populate(['initiator', 'targetUsers'])
+        .populate([{path: 'targetUsers', model: User}, 'initiator'])
     ctx.success(moneyChanges)
     return next()
 }
