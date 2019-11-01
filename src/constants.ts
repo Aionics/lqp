fe4649bec46609491ad424c8850577de77420aee
ArtemK <artem.kosarev@ibm.com>
2019-11-01 19:53:37 +0300

    constants fix

--- src/constants.ts
+++ src/constants.ts
@@ -5,11 +5,11 @@ export const IS_PRODUCTION = process.env.NODE_ENV !== 'development'
 export const SESSION_COOKIE_KEY = 'secret_key'
 export const SESSION_COOKIE_MAX_AGE_MS = 1000 * 60 * 60 * 24 * 7
 
-export const INCOME_INTERVAL = 1000 * 60 * 5
-export const INCOME_AMOUNT = 5
+export const INCOME_INTERVAL = 1000 * 60 * 1
+export const INCOME_AMOUNT = 1
 export const WELCOME_BONUS = 10
 export const LOOTBOX_COSTS = {
-    1: -5,
-    2: -20,
-    3: -50
+    1: -15,
+    2: -30,
+    3: -100
 }
